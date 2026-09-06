87ff22e12d9f1c098fe38f1dae06e95b226744f9
Song seunghyun <ssh12960@gmail.com>
2026-09-06 22:08:37 +0900

    add: 절댓값힙

---
+++ week07/절댓값힙/seunghyun518.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+            if(absA == absB){
+                return a - b;
+            }
+            return absA - absB;
+        });
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            int x = Integer.parseInt(br.readLine());
+
+            if(x != 0){
+                pq.offer(x);
+            }
+            else{
+                if(pq.isEmpty()){
+                    System.out.println("0");
+                }
+                else{
+                    System.out.println(pq.poll());
+                }
+            }
+        }
+    }
+}
